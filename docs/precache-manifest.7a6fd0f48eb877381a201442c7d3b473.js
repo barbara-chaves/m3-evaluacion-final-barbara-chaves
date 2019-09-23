45e5a980da7742231dd0eb8195cdfb5697c23ad3
barbara-chaves <barbarabarcellos90@gmail.com>
2019-09-24 00:29:34 +0200

    create-docs

---
+++ docs/precache-manifest.7a6fd0f48eb877381a201442c7d3b473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c934e66250dc188af556b14349cb96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fc527ef5c1ee85c683",
+    "url": "/static/css/main.9a70ec48.chunk.css"
+  },
+  {
+    "revision": "bae1f73643d0fc6ae41c",
+    "url": "/static/js/2.ce136b08.chunk.js"
+  },
+  {
+    "revision": "89fc527ef5c1ee85c683",
+    "url": "/static/js/main.3b57e713.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/static/media/Rick_and_Morty.75509f2e.png"
+  }
+]);
