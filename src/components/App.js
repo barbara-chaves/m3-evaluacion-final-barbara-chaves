546c0ec92bef27ecb9c87b138afe171ccb462d20
barbara-chaves <barbarabarcellos90@gmail.com>
2019-09-23 15:41:06 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../stylesheets/app.css';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="app">
+        
+      </div>
+    );
+
+  }  
+    
+    
+}
+
+export default App;
