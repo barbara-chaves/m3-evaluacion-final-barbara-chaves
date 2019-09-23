537c3dcf3116aba54cef5c41a51a83868a966f3f
barbara-chaves <barbarabarcellos90@gmail.com>
2019-09-23 18:58:04 +0200

    insert fa icons

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDizzy, faSmile, faQuestionCircle} from '@fortawesome/free-regular-svg-icons'
+import { faRedditAlien } from '@fortawesome/free-brands-svg-icons'
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
 
 const CharacterDetails = props => {
   const {name, species, origin, image, status, episodes} = props.char;
+  const changeStatus = () => {
+    if (status === 'Alive') {
+      return <FontAwesomeIcon icon={faSmile} title='Alive'/>
+    } else if (status === 'Dead') {
+      return <FontAwesomeIcon icon={faDizzy} title='Dead'/>
+    } 
+    return <FontAwesomeIcon icon={faQuestionCircle} title='Unknown'/>
+  }
+
+  const changeSpecie = () => {
+    if (species === 'Alien') {
+      return <FontAwesomeIcon icon={faRedditAlien} title='Alien'/>
+    } else if (species === 'Human') {
+      return <FontAwesomeIcon icon={faUserAlt} title='Human'/>
+    } 
+    return <FontAwesomeIcon icon={faQuestionCircle} title='Unknown'/>
+  }
+  
   return (
     <div className='details'>
       <Link to='/'>
@@ -16,8 +38,8 @@ const CharacterDetails = props => {
         <h2 className='details__name'>
           {name}
         </h2>
-        <p className='details__status'><span className='details___names'>Status: </span>{status}</p>
-        <p className='details__species'><span className='details___names'>Species: </span>{species}</p>
+        <p className='details__status'><span className='details___names'>Status: </span>{changeStatus()}</p>
+        <p className='details__species'><span className='details___names'>Species: </span>{changeSpecie()}</p>
         <p className='details__origin'><span className='details___names'>Origin: </span>{origin}</p>
         <p className='details__episodes'><span className='details___names'>Episodes: </span>{episodes}</p>
       </div>
